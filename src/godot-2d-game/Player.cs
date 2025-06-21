cc7c803ec8797b09032949f73074e24f233da543
Lee-Roy Ashworth <leeroya@gmail.com>
2025-06-21 13:42:10 +0200

    chore: removed bin feat: enabled the moving of the character

--- src/godot-2d-game/Player.cs
+++ src/godot-2d-game/Player.cs
@@ -5,4 +5,50 @@ public partial class Player : Area2D
     public int Speed { get; set; } = 400;
 
     public Vector2 ScreenSize;
+    
+    public override void _Ready()
+    {
+        // Get the screen size
+        ScreenSize = GetViewport().GetVisibleRect().Size;
+    }
+    
+    public override void _Process(double delta)
+    {
+        // Get the input direction
+        var velocity = Vector2.Zero;
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed * (float)delta;
+            animatedSprite2D.Play();
+        }
+        else
+        {
+            animatedSprite2D.Stop();
+        }
+        
+        Position += velocity * (float)delta;
+        Position = new Vector2(
+           x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+           y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
+    }
 }
